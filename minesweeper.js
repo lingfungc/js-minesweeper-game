8fe617e6f501ee881d0176a003ca8c6fb550a82b
Ling Fung Chan <justinlfchan@gmail.com>
2023-02-16 18:07:35 +0000

    added createBoard() function

--- minesweeper.js
+++ minesweeper.js
@@ -1,1 +1,20 @@
 console.log("Check Connection");
+
+// We make sure all of our HTML code is loaded before reading this JavaScript code
+// Actually it has the same effect to put <script src="xxx.js"></script> in the end of the HTML file
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  let width = 10;
+  let squares = [];
+
+  // Create a Board
+  function createBoard() {
+    for (let i = 0; i < width * width; i++) {
+      const square = document.createElement("div");
+      square.setAttribute("id", i);
+      grid.appendChild(square);
+      squares.push(square);
+    }
+  }
+  createBoard();
+});
